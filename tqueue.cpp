fea09f6f7022e5e87c7c9cd2aca7e39d80184024
Matheus Saldanha <matheus.saldanha@usp.br>
2017-07-13 00:00:43 -0300

    Improve TQueue for allowing peek()

--- tqueue.cpp
+++ tqueue.cpp
@@ -4,21 +4,29 @@ TQueue::TQueue(std::istream &srcfile, int offset, int nRecords)
 	: d_file(srcfile), d_offset(offset),
 	  d_nRecords(nRecords), d_nRead(0)
 {
+	readNext();
 }
 
 bool TQueue::empty(){
 	return d_nRead == d_nRecords ? true : false;
 }
 
-Person TQueue::next(){
-	// TODO: Buffering of Person objects
-
-	if(empty()){
-		return Person("", -1); // Not going to throw any exception
-	} else {
-		Person p;
+void TQueue::readNext(){
+	if(!empty()) {
 		d_file.seekg(d_offset);
-		p.read(d_file);
-		return p;
+		d_person.read(d_file);
+		d_offset = d_file.tellg();
 	}
+}
+
+Person TQueue::next(){
+	Person p = std::move(d_person);
+
+	readNext();
+
+	return p;
+}
+
+Person &TQueue::peek(){
+	return d_person;
 }
--- tqueue.h
+++ tqueue.h
@@ -17,10 +17,23 @@ class TQueue {
 	// Number of records read so far.
 	int d_nRead;
 
+	// Next person on the queue
+	Person d_person;
+
+	// Retrieves the next person on the file
+	void readNext();
+
 public:
 	TQueue(std::istream &srcfile, int offset, int nRecords);
+
+	// Returns True if there are no more Person objects in this queue.
 	bool empty();
+
+	// Returns the next Person, and removes it from the queue.
 	Person next();
+
+	// Returns the next Person, but does not remove it from the queue.
+	Person &peek();
 };
 
 #endif // TQUEUE_H
